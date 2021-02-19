59dfa19c6446f4aaae42235c4105554b5879b5fd
Yoiro <degrevesim@gmail.com>
2021-02-19 12:52:42 +0100

    test: add test triggering 500 and improved other tests

--- test/test_productionplan.py
+++ test/test_productionplan.py
@@ -8,21 +8,26 @@ __PRODUCTION_PLAN_URI = "http://localhost:8888/productionplan/"
 def test_windy_input(windy):
     r = requests.post(f"{__PRODUCTION_PLAN_URI}", json=windy)
     assert r.status_code == 200
-    print(r.json())
     assert compute_load(r.json()) == windy["load"]
 
 def test_no_wind_input(no_wind):
     r = requests.post(f"{__PRODUCTION_PLAN_URI}", json=no_wind)
     assert r.status_code == 200
+    assert compute_load(r.json()) == no_wind["load"]
 
 def test_high_load(high_load):
     r = requests.post(f"{__PRODUCTION_PLAN_URI}", json=high_load)
     assert r.status_code == 200
+    assert compute_load(r.json()) == high_load["load"]
 
 def test_empty_payload():
     r = requests.post(f"{__PRODUCTION_PLAN_URI}", json={})
     assert r.status_code == 400
 
-def test_None_payload():
+def test_none_payload():
     r = requests.post(f"{__PRODUCTION_PLAN_URI}", json=None)
     assert r.status_code == 400
+
+def test_missing_keys():
+    r = requests.post(f"{__PRODUCTION_PLAN_URI}", json={"load": 0})
+    assert r.status_code == 500
